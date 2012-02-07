e04c56df2f61b967cb0be8cb5969f65cfb47ac79
Jerry Lundström <jerry.lundstrom@iis.se>
2012-02-07 10:11:08 +0000

    Removing the old test framework

--- test-dnsadapters.d/00-start-stop/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ods_reset_env &&
-log_this ods-control-start ods-control start &&
-syslog_waitfor 60 'ods-signerd: \[engine\] signer started' &&
-syslog_grep 'ods-enforcerd: Sleeping for' &&
-log_this ods-control-stop ods-control stop &&
-syslog_waitfor 60 'ods-signerd: \[engine\] signer shutdown' &&
-syslog_grep 'ods-enforcerd: all done'
