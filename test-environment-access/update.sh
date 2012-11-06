50c650343e5c5db0e1b78680f41dafa28c12f2f8
Jerry Lundström <jerry.lundstrom@iis.se>
2012-11-06 15:09:48 +0000

    Use our own ca bundle, added more checks and cleanups.

--- test-environment-access/update.sh
+++ test-environment-access/update.sh
@@ -31,28 +31,43 @@ fi
 case "$1" in
   update )
     rm -f -- "$HOME/test-environment-access/tmp/update.sh.$$" &&
-    wget -O "$HOME/test-environment-access/tmp/update.sh.$$" "https://svn.opendnssec.org/trunk/testing/test-environment-access/update.sh" &&
+    wget --ca-certificate="$HOME/test-environment-access/startssl-ca-bundle.pem" -O "$HOME/test-environment-access/tmp/update.sh.$$" "https://svn.opendnssec.org/trunk/testing/test-environment-access/update.sh" &&
     exec sh -x "$HOME/test-environment-access/tmp/update.sh.$$" authorized_keys ||
     {
       echo "Update failed, exit."
+      rm -f -- "$HOME/test-environment-access/tmp/update.sh.$$"
       exit 6
     }
     ;;
   authorized_keys )
     if [ "$0" != "$HOME/test-environment-access/update.sh" ]; then
+      mv "$HOME/test-environment-access/update.sh" "$HOME/test-environment-access/update.sh.backup" ||
+      {
+        echo "Unable to backup current update.sh, exit."
+        exec sh -x "$HOME/test-environment-access/update.sh" clean "$$" 10
+        exit 10
+      }
       cp -p -- "$0" "$HOME/test-environment-access/update.sh" ||
       {
         echo "Installing new update.sh failed, exit."
+        mv "$HOME/test-environment-access/update.sh.backup" "$HOME/test-environment-access/update.sh" ||
+        {
+          echo "Unable to restore backup of current update.sh, exit."
+          exit 11
+        }
+        exec sh -x "$HOME/test-environment-access/update.sh" clean "$$" 7
         exit 7
       }
     fi
     rm -f -- "$HOME/test-environment-access/tmp/authorized_keys.$$" &&
-    wget -O "$HOME/test-environment-access/tmp/authorized_keys.$$" "https://svn.opendnssec.org/trunk/testing/test-environment-access/authorized_keys" &&
+    wget --ca-certificate="$HOME/test-environment-access/startssl-ca-bundle.pem" -O "$HOME/test-environment-access/tmp/authorized_keys.$$" "https://svn.opendnssec.org/trunk/testing/test-environment-access/authorized_keys" &&
     chmod 640 "$HOME/test-environment-access/tmp/authorized_keys.$$" &&
     mv -- "$HOME/test-environment-access/tmp/authorized_keys.$$" "$HOME/.ssh/authorized_keys" &&
     exec sh -x "$HOME/test-environment-access/update.sh" clean "$$" ||
     {
       echo "Installing new authorized_keys failed, exit."
+      rm -f -- "$HOME/test-environment-access/tmp/authorized_keys.$$"
+      exec sh -x "$HOME/test-environment-access/update.sh" clean "$$" 8
       exit 8
     }
     ;;
@@ -63,6 +78,9 @@ case "$1" in
         echo "Clean up failed, exit."
         exit 9
       }
+    fi
+    if [ "$3" -ge 0 ] 2>/dev/null; then
+      exit "$3"
     fi
     ;;
 esac
