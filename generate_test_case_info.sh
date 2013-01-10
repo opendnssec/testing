d6085626425a1559e9edf5175ab1a9595e638f80
Sara Dickinson <sara@sinodun.com>
2013-01-10 14:32:19 +0000

    TEST: Remove the #CATEGORY comment from the tests. The test name should be self explanatory.

--- generate_test_case_info.sh
+++ generate_test_case_info.sh
@@ -18,12 +18,11 @@ generate_test_info() {
 	fi
 	# Replace any commas with dashes since we will write to a csv file	
 	test_description=`grep "^#TEST:" test.sh | sed s/#TEST://g | sed s/,/-/g` 
-	test_category=`grep "^#CATEGORY:" test.sh | sed s/#CATEGORY://g | sed s/,/-/g`
 	# Just extract the issue identifier
 	test_issues=`grep "^#OPENDNSSEC\|^#SUPPORT" test.sh | sed s/#//g | sed 's/ .*//' `
 	test_todo=`grep "^#TODO:" test.sh | sed s/#TODO://g | sed s/,/-/g` 
 	
- 	echo $1","$test_path","$test_status","$test_category","$test_description","$test_issues","$test_todo >> $OUTPUT_FILE
+ 	echo $1","$test_path","$test_status","$test_description","$test_issues","$test_todo >> $OUTPUT_FILE
 
 }
 
@@ -49,7 +48,7 @@ pwd=`pwd`
 echo "Searching top level directory "$pwd" for tests"
 OUTPUT_FILE=$pwd/$OUTPUT_FILE_NAME
 rm $OUTPUT_FILE
-echo "Directory,Name,Status,Category,Description,Issues,To do" > $OUTPUT_FILE
+echo "Directory,Name,Status,Description,Issues,To do" > $OUTPUT_FILE
 
 for LOCAL_TEST_DIR in test-cases.d test-cases-daily.d test-cases-weekly.d; do
 
