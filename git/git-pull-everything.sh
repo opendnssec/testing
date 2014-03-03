4a83d5d84c52934b6dbc05f5cbfc6e1e0a5180a8
Jakob Schlyter <jakob@kirei.se>
2014-03-03 09:58:33 +0100

    more scripts

--- git/git-pull-everything.sh
+++ git/git-pull-everything.sh
@@ -2,7 +2,19 @@
 
 # Fetch updates from upstream
 git fetch upstream
- 
+
+# Update your local master branch
+git checkout master &&
+git merge --ff-only upstream/master &&
+git push origin master &&
+echo "master pull OK" || echo "master pull FAILED"
+
+# Update your local develop branch
+git checkout develop &&
+git merge --ff-only upstream/develop &&
+git push origin develop &&
+echo "develop pull OK" || echo "develop pull FAILED"
+
 # If this is a fork of OpenDNSSEC, also update all MAJOR VERSION branches
 git branch -r | grep 'origin/[0-9].[0-9]/' | sed 's%origin/%%' | while read branch; do \
 	git checkout $branch || git checkout -b $branch origin/$branch || break; \
